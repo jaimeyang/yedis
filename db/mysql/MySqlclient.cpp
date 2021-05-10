13f3b1e664d2bd43335afeda273fe931b05d861d
jaimeyang1990@gmail.com <AybTejLiOtotpe6>
2021-05-11 00:26:41 +0800

    add create table and index

--- db/mysql/MySqlclient.cpp
+++ db/mysql/MySqlclient.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 
 bool yedis::MySqlclient::connect(const string &host, const string &user, const string &pwd, const string &db, unsigned int port) {
+    cout<<"connect c"<<endl;
     mysql_init(&this->m_mysql);
     if (!mysql_real_connect(&this->m_mysql,host.c_str(),user.c_str(),pwd.c_str(),db.c_str(), 0, NULL, 0)) {
         fprintf(stderr, "Failed to connect to database: Error: %s\n",
@@ -17,7 +18,15 @@ bool yedis::MySqlclient::connect(const string &host, const string &user, const s
 int yedis::MySqlclient::crateTable(const string& str) {
     auto ret = mysql_real_query(&this->m_mysql,str.c_str(),str.size());
     if (ret != 0) {
-        cout<<"mysql crateTable is error "<<ret<<endl;        
+        cout<<"mysql crateTable is error "<<mysql_error(&this->m_mysql)<<endl;        
+    }
+    return ret;
+}
+
+int yedis::MySqlclient::createIndex(const string& sql) {
+    auto ret = mysql_real_query(&this->m_mysql,sql.c_str(),sql.size());
+    if (ret != 0) {
+        cout<<"mysql createIndex is error "<<mysql_error(&this->m_mysql)<<endl;        
     }
     return ret;
 }
--- db/mysql/MySqlclient.h
+++ db/mysql/MySqlclient.h
@@ -17,6 +17,7 @@ namespace yedis
         
         bool connect(const string &host, const string &user, const string &pwd, const string &db, unsigned int port);
         int crateTable(const string& sql);
+        int createIndex(const string& sql);
         int query(const string& sql);
     private:
         /* data */
